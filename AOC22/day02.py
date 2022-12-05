276ee93b137d5e2ea341110298b3a25b3ba271fa
Tristan Teasdale <tristan.teasdale@hexagon.com>
2022-12-05 08:44:58 +0000

    Day02 pt 2 solution

--- AOC22/day02.py
+++ AOC22/day02.py
@@ -4,43 +4,74 @@ TEST_DATA = """A Y
 B X
 C Z"""
 
-def win_or_lose(round):
-    them = round[0]
-    me = round[2]
+def win_or_lose(them, me):
+    # them = round[0]
+    # me = round[2]
 
     if them == 'A': # rock
-        if me == 'X': return 3 # rock
-        if me == 'Y': return 6 # paper
-        if me == 'Z': return 0 # scissors
+        if me == 'A': return 3 # rock
+        if me == 'B': return 6 # paper
+        if me == 'C': return 0 # scissors
 
     if them == 'B': # paper
-        if me == 'X': return 0 # rock
-        if me == 'Y': return 3 # paper
-        if me == 'Z': return 6 # scissors
+        if me == 'A': return 0 # rock
+        if me == 'B': return 3 # paper
+        if me == 'C': return 6 # scissors
 
     if them == 'C': # scissors
-        if me == 'X': return 6 # rock
-        if me == 'Y': return 0 # paper
-        if me == 'Z': return 3 # scissors
+        if me == 'A': return 6 # rock
+        if me == 'B': return 0 # paper
+        if me == 'C': return 3 # scissors
 
-def calculate_score(puz_input):
+
+def calculate_score(match_ups):
     score = 0
 
-    for round in puz_input:
-        round_score = win_or_lose(round)
+    for round in match_ups:
+        them = round[0]
+        me = round[1]
+        round_score = win_or_lose(them, me)
 
-        if round[2] == 'X': round_score += 1
-        if round[2] == 'Y': round_score += 2
-        if round[2] == 'Z': round_score += 3
+        if me == 'A': round_score += 1
+        if me == 'B': round_score += 2
+        if me == 'C': round_score += 3
 
         score += round_score
 
     return score
 
+
+def create_match_ups(round):
+    them = round[0]
+    expected = round[2]
+    me = ''
+
+    if expected == 'X': # lose
+        if   them == 'A': me = 'C'
+        elif them == 'B': me = 'A'
+        elif them == 'C': me = 'B'
+
+    elif expected == 'Y': # draw
+        if   them == 'A': me = 'A'
+        elif them == 'B': me = 'B'
+        elif them == 'C': me = 'C'
+
+    elif expected == 'Z': # win
+        if   them == 'A': me = 'B'
+        elif them == 'B': me = 'C'
+        elif them == 'C': me = 'A'
+
+    return (them, me)
+
+def create_strategy(puz_input):
+    match_ups = [create_match_ups(x) for x in puz_input]
+
+    return calculate_score(match_ups)
+
 if __name__ == "__main__":
-    # puz_input = common.load_test_data(TEST_DATA)
+    #puz_input = common.load_test_data(TEST_DATA)
     puz_input = common.load_puzzle_input("data/day02.txt")
 
-    score = calculate_score(puz_input)
+    score = create_strategy(puz_input)
 
     print(score)
