adaa93b438ac129da61079abd2e8efea99c21901
Tristan Teasdale <tristan.teasdale@hexagon.com>
2023-12-11 10:30:15 +0000

    Solution day02 pt2

--- AOC23/day02.py
+++ AOC23/day02.py
@@ -8,6 +8,7 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
 def find_possible_games(games):
     id_total = 0
+    power_total = 0
 
     for game in games:
         game_data = game.split(': ')
@@ -16,27 +17,49 @@ def find_possible_games(games):
         game_data[1] = [x.split(', ') for x in game_data[1]]
         
         game_valid = True
+        max_red = 0
+        max_green = 0
+        max_blue = 0
 
         for hand in game_data[1]:
             for cubes in hand:
                 cubes = cubes.split(' ')
-                if game_valid:
-                    if (cubes[1] == "red" and int(cubes[0]) > 12) \
-                    or (cubes[1] == "green" and int(cubes[0]) > 13) \
-                    or (cubes[1] == "blue" and int(cubes[0]) > 14):
+
+                num_cubes = int(cubes[0])
+                    
+                if cubes[1] == "red":
+                    if num_cubes > 12:
                         game_valid = False
-                        break
+
+                    if num_cubes > max_red:
+                        max_red = num_cubes
+
+                elif cubes[1] == "green":
+                    if num_cubes > 13:
+                        game_valid = False
+
+                    if num_cubes > max_green:
+                        max_green = num_cubes
+
+                elif cubes[1] == "blue":
+                    if num_cubes > 14:
+                        game_valid = False
+
+                    if num_cubes > max_blue:
+                        max_blue = num_cubes
 
         if game_valid:
             id = int(game_data[0][1])
             id_total += id
 
-    return id_total
+        power = (max_red * max_green * max_blue)
+        power_total += power
+
+    print(f"Sum of IDs for valid games: {id_total}")
+    print(f"Sum of powers: {power_total}")
 
 if __name__ == "__main__":
     #puz_input = common.load_test_data(TEST_DATA)
     puz_input = common.load_puzzle_input("data/day02.txt")
 
-    result = find_possible_games(puz_input)
-
-    print(result)
+    find_possible_games(puz_input)
