2af462fe3023605285f0fb203d8742ece37832c1
Tristan Teasdale <Tristan.Teasdale@malvernpanalytical.com>
2024-09-17 16:09:31 +0100

    Solution day 4 pt 2

--- AOC23/day04.py
+++ AOC23/day04.py
@@ -7,6 +7,7 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
+
 def find_total_points(cards):
     total_points = 0
 
@@ -15,8 +16,19 @@ def find_total_points(cards):
 
     print(total_points)
 
+
 def calculate_points(card):
-    card = card.replace("  ", " ") # replace double spaces with one space
+    num_matches = find_number_of_matches(card)
+
+    if num_matches == 0:
+        return 0
+
+    points = 2 ** (num_matches - 1)
+    return points
+
+
+def find_number_of_matches(card):
+    card = card.replace("  ", " ")  # replace double spaces with one space
 
     just_numbers = card.split(": ")[1]
     numbers = just_numbers.split(" | ")
@@ -29,17 +41,28 @@ def calculate_points(card):
     matches = winning_nums.intersection(my_nums)
     num_matches = len(matches)
 
-    if num_matches == 0:
-        return 0
-    else:
-        points = 2 ** (len(matches) - 1)
-        return points
+    return num_matches
+
+
+def find_total_scratchcards(card_data):
+    cards = [[card, 1] for card in card_data]
+
+    for i, card in enumerate(cards):
+
+        for _ in range(card[1]):
+            num_matches = find_number_of_matches(card[0])
+
+            for x in range(i + 1, i + num_matches + 1):
+                cards[x][1] += 1
+
+    # algorithm is slow due to many loops, works but needs improving
+    total_cards = sum([c[1] for c in cards])
+    print(f"Total cards: {total_cards}")
+
 
 if __name__ == "__main__":
-    #puz_input = common.load_test_data(TEST_DATA)
+    # puz_input = common.load_test_data(TEST_DATA)
     puz_input = common.load_puzzle_input("data/day04.txt")
 
-    find_total_points(puz_input)
-
-
-
+    # find_total_points(puz_input)
+    find_total_scratchcards(puz_input)
