68a95709535ec229f64b278e913e030a13916f0b
Tristan Teasdale <Tristan.Teasdale@malvernpanalytical.com>
2024-12-03 09:50:59 +0000

    Day 2 solution

--- AoC24/day02.py
+++ AoC24/day02.py
@@ -12,35 +12,59 @@ def count_safe(reports):
     safe_count = 0
 
     for report in reports:
-        is_increasing = False
-        is_safe = True
-        previous = report[0]
-
-        for i, level in enumerate(report):
-            if i == 0:
-                continue
-
-            if i == 1:
-                is_increasing = level > previous
-
-            diff = abs(level - previous)
-
-            if diff < 1 or diff > 3:
-                is_safe = False
-                break
-
-            if (is_increasing and level < previous) or (
-                not is_increasing and level > previous
-            ):
-                is_safe = False
-                break
-
-            previous = level
-
-        if is_safe:
+        if report_is_safe(report):
             safe_count += 1
+        else:
+            is_safish = apply_problem_dampener(report)
+
+            if is_safish:
+                safe_count += 1
 
     print("Safe count: " + str(safe_count))
+
+
+def apply_problem_dampener(report):
+    is_safeish = True
+
+    for i in range(len(report)):
+        damped_report = report.copy()
+        damped_report.pop(i)
+
+        is_safeish = report_is_safe(damped_report)
+
+        if is_safeish:
+            break
+
+    return is_safeish
+
+
+def report_is_safe(report):
+    is_increasing = False
+    is_safe = True
+    previous = report[0]
+
+    for i, level in enumerate(report):
+        if i == 0:
+            continue
+
+        if i == 1:
+            is_increasing = level > previous
+
+        diff = abs(level - previous)
+
+        if diff < 1 or diff > 3:
+            is_safe = False
+            break
+
+        if (is_increasing and level < previous) or (
+            not is_increasing and level > previous
+        ):
+            is_safe = False
+            break
+
+        previous = level
+
+    return is_safe
 
 
 if __name__ == "__main__":
