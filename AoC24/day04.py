d43fb05b43191798591f82e7dd87c45a3c5c7e84
Tristan Teasdale <Tristan.Teasdale@malvernpanalytical.com>
2024-12-11 09:11:59 +0000

    Day 4 pt 2 solution

--- AoC24/day04.py
+++ AoC24/day04.py
@@ -12,11 +12,11 @@ MAMMMXMMMM
 MXMXAXMASX"""
 
 
-def find_xs(grid):
+def find_char_points(grid, char):
     x_points = []
     for y, row in enumerate(grid):
         for x, column in enumerate(row):
-            if column == "X":
+            if column == char:
                 x_points.append((x, y))
 
     return x_points
@@ -89,7 +89,7 @@ def find_xmas(grid):
     max_x = len(grid[0]) - 1
     max_y = len(grid) - 1
 
-    x_points = find_xs(grid)
+    x_points = find_char_points(grid, "X")
 
     for x_point in x_points:
         surrounding_points = get_surrounding_points(x_point, max_x, max_y)
@@ -119,8 +119,56 @@ def find_xmas(grid):
     print("XMAS count: " + str(xmas_count))
 
 
+def find_x_mas(grid):
+    x_mas_count = 0
+    max_x = len(grid[0]) - 1
+    max_y = len(grid) - 1
+
+    a_points = find_char_points(grid, "A")
+    filtered_points = filter(
+        lambda point: (point[0] > 0 and point[0] < max_x)
+        and (point[1] > 0 and point[1] < max_y),
+        a_points,
+    )  # filter out the A's on the outer edge of the grid
+
+    for a_point in filtered_points:
+        x, y = a_point
+
+        top_left = grid[y - 1][x - 1]
+        top_right = grid[y - 1][x + 1]
+        bottom_left = grid[y + 1][x - 1]
+        bottom_right = grid[y + 1][x + 1]
+
+        # 'MAS' diagonal down right
+        if top_left == "M" and bottom_right == "S":
+            if (top_right == "M" and bottom_left == "S") or (
+                bottom_left == "M" and top_right == "S"
+            ):
+                x_mas_count += 1
+        # 'MAS' diagonal down left
+        elif top_right == "M" and bottom_left == "S":
+            if (top_left == "M" and bottom_right == "S") or (
+                bottom_right == "M" and top_left == "S"
+            ):
+                x_mas_count += 1
+        # 'MAS' diagonal up left
+        elif bottom_right == "M" and top_left == "S":
+            if (bottom_left == "M" and top_right == "S") or (
+                top_right == "M" and bottom_left == "S"
+            ):
+                x_mas_count += 1
+        # 'MAS' diagonal up right
+        elif top_right == "M" and top_right == "S":
+            if (top_left == "M" and bottom_right == "S") or (
+                bottom_right == "M" and top_left == "S"
+            ):
+                x_mas_count += 1
+
+    print("X-MAS count: " + str(x_mas_count))
+
+
 if __name__ == "__main__":
     # puz_input = common.load_test_data(TEST_DATA)
     puz_input = common.load_puzzle_input("data/day04.txt")
 
-    find_xmas(puz_input)
+    find_x_mas(puz_input)
